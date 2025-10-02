698697e7612f226021b4ef310475e2e0c6756b3c
WCoopW <artemmonakov@rambler.ru>
2025-10-03 02:05:48 +0300

    chore: setup fvm

---
+++ lib/src/core/constant/localization/translations/app_localizations_en.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'rick_and_morty';
+
+  @override
+  String get custom_colors => 'Custom Colors';
+
+  @override
+  String get default_themes => 'Default Themes';
+
+  @override
+  String get locales => 'Locales';
+}
